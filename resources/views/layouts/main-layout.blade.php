104d6e4b7c70500f889ecd915bbefe8e89d956d7
Andrea <miranda.andrea.nd@gmail.com>
2020-09-24 17:05:40 +0200

    layout, partials

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Controller + DB</title>
+  </head>
+  <body>
+    <div class="container">
+      @include('partials.header')
+      @yeld('content')
+      @include('parials.footer')
+
+    </div>
+  </body>
+</html>
